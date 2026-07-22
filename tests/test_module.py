b9f3b7b435b24ebadabd48e647abfe86cc04e2c8
Luciano Rossi <lukio@gcoop.coop>
2026-07-22 17:37:40 -0300

    refactor: update tests to Tryton 8.0 pattern
    - Clean __init__.py to only LICENSE header
    - Rename tests.py to test_module.py (ModuleTestCase)
    - Add test_scenario.py for doctest loading

---
+++ tests/test_module.py
@@ -0,0 +1,8 @@
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class CooperativeCashflowArTestCase(ModuleTestCase):
+    'Test cooperative_cashflow_ar module'
+    module = 'cooperative_cashflow_ar'
+
+del ModuleTestCase
